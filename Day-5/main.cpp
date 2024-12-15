ab390dc4c651e942a9c3073e8895018c1482a494
Ksawery Kuczynski <ksawerykuczynskikk@gmail.com>
2024-12-15 13:57:40 +0100

    feat: Completed Day 5 part 2

--- Day-5/main.cpp
+++ Day-5/main.cpp
@@ -17,31 +17,71 @@ vector<string> getNums(string line) {
     return nums;
 }
 
-int partOne(fstream& fh, map<string, set<string>>& order) {
-    int validMiddleSum = 0;
-    string line;
-    while (getline(fh, line)) { 
-        auto nums = getNums(line);
-        bool count = true;
-        for (int start = 0; start < nums.size(); start++) {
-            auto currNext = order[nums[start]];
+bool isLineValid(vector<string>& nums, map<string, set<string>>& order) {
+    for (int start = 0; start < nums.size(); start++) {
+        auto currNext = order[nums[start]];
 
-            for (int next = start + 1; next < nums.size(); next++) {
-                if (currNext.find(nums[next]) == currNext.end()) {
-                    // invalidate this line
-                    start = nums.size();
-                    count = false;
-                    break;
-                }
+        for (int next = start + 1; next < nums.size(); next++) {
+            if (currNext.find(nums[next]) == currNext.end()) {
+                return false;
             }
         }
+    }
 
-        if (count) {
+    return true;
+}
+
+void reorderNums(vector<string>& nums, map<string, set<string>>& order) {
+    set<string> currNums(nums.begin(), nums.end());
+    vector<string> ordered(nums.size());
+    for (auto num : nums) {
+        vector<string> intersections{};
+        set_intersection(currNums.begin(), currNums.end(), order[num].begin(), order[num].end(), back_inserter(intersections));
+
+        ordered[nums.size() - intersections.size() - 1] = num;
+    }
+
+    // assign ordered strings to nums
+    for (int i = 0; i < nums.size(); i++) {
+        nums[i] = ordered[i];
+    }
+}
+
+int partTwo(vector<string>& lines, map<string, set<string>>& order) {
+    int validMiddleSum = 0;
+    for (auto line : lines) { 
+        auto nums = getNums(line);
+    
+        if (!isLineValid(nums, order)) {
+            // reorder
+            reorderNums(nums, order);
             validMiddleSum += stoi(nums[int(nums.size() / 2)]);
         }
     }
 
     return validMiddleSum;
+}
+int partOne(vector<string>& lines, map<string, set<string>>& order) {
+    int validMiddleSum = 0;
+    for (auto line : lines) { 
+        auto nums = getNums(line);
+    
+        if (isLineValid(nums, order)) {
+            validMiddleSum += stoi(nums[int(nums.size() / 2)]);
+        }
+    }
+
+    return validMiddleSum;
+}
+
+vector<string> readLines(fstream& fh) { 
+    string line;
+    vector<string> lines{};
+    while (getline(fh, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
 }
 
 map<string, set<string>> readOrder(fstream& fh) {
@@ -68,7 +108,9 @@ int main() {
     fstream fh;
     fh.open("input.txt");
     auto order = readOrder(fh);
-    cout << partOne(fh, order) << endl;
+    auto lines = readLines(fh);
+    cout << "Part 1: " << partOne(lines, order) << endl;
+    cout << "Part 2: " << partTwo(lines, order) << endl;
     fh.close();
     return 0;
 }
